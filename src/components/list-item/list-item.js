94037c80b1d554c948fa57e24edf34f35902666e
Volodya Shufliak <v.shufliak.work@gmail.com>
2023-05-09 21:55:21 +0300

    add styles for listitem text

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -7,12 +7,17 @@ const ListItem = (props) => {
     const useNotesContext = useContext(notesContext)
     const clazz = props.note.id===useNotesContext.activeNoteId?'list_item_container active_note':'list_item_container';
     function formatDateForListItem(date) {
+        if (date.toLocaleDateString('en-ca') === new Date().toLocaleDateString('en-ca')) {
+            const datetime =date.toLocaleTimeString('en-US').split(' ');
+            const datetime_without_seconds = datetime[0].split(':');
+            return `${datetime_without_seconds[0]}:${datetime_without_seconds[1]} ${datetime[1]}`;
+        }
         return date.toLocaleDateString('en-ca').split('-').reverse().join('/');
     }
     return(
         <div className={clazz} onClick={()=>useNotesContext.showNote(props.note.id)}>
             <div className="title">{props.note.title}</div>
-            <div className="modifided">{formatDateForListItem(props.note.date)}</div>
+            <div className="modifided">{formatDateForListItem(props.note.date)} {props.note.description}</div>
         </div>
     )
 }
